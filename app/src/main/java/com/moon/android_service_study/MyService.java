1500e5b677a4723ce266aa90f9605d9de7b275b4
문 성 운 <mkik90911@naver.com>
2022-02-04 16:21:41 +0900

    MyService.java Create and Manifest.xml <Service> Tag Add

---
+++ app/src/main/java/com/moon/android_service_study/MyService.java
@@ -0,0 +1,16 @@
+package com.moon.android_service_study;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
