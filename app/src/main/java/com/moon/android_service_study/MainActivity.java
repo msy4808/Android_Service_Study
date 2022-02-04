c9537449507b3a3283b0f37c174a50a49604ad6f
문 성 운 <mkik90911@naver.com>
2022-02-04 17:23:54 +0900

    MyService.java onStartCommand TEST

--- app/src/main/java/com/moon/android_service_study/MainActivity.java
+++ app/src/main/java/com/moon/android_service_study/MainActivity.java
@@ -2,13 +2,32 @@ package com.moon.android_service_study;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editText;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str = editText.getText().toString();
+
+                Intent intent = new Intent(getApplicationContext(),MyService.class); //인텐트 객체 생성
+
+                intent.putExtra("command","show"); //생성한 인텐트 객체에 부가 데이터들 넣어주기
+                intent.putExtra("value",str);
+                startService(intent); //서비스 시작하기MyService 클래스에 onStartCommand()메소드로 전달
+            }
+        });
     }
 }
--- app/src/main/java/com/moon/android_service_study/MyService.java
+++ app/src/main/java/com/moon/android_service_study/MyService.java
@@ -2,19 +2,45 @@ package com.moon.android_service_study;
 
 import android.app.Service;
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.IBinder;
+import android.util.Log;
 
 public class MyService extends Service {
+    private static final String TAG = "MyService : ";
+
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.d(TAG,"onCreate() 호출됨");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG,"onStartCommand() 호출됨");
+        if(intent == null){
+            return Service.START_STICKY; //null이라면 서비스 재시작
+        }else{ //intent가 null이 아니면 processCommand메서드 호출하기
+            processCommand(intent, flags, startId);
+        }
         return super.onStartCommand(intent, flags, startId);
     }
 
+    private void processCommand(Intent intent, int flags, int startId){
+        String command = intent.getStringExtra("command");
+        String value = intent.getStringExtra("value");
+        Log.d(TAG,"command : " + command + "| value : " + value);
+
+        for(int i = 0;i < 5; i++){
+            try{
+                Thread.sleep(1000);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            Log.d(TAG,"Waiting : " + i + "sec");
+        }
+
+    }
     @Override
     public void onDestroy() {
         super.onDestroy();
