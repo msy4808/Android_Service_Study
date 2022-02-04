16528181b5f413cbb79dd810be544804d2f44462
문 성 운 <mkik90911@naver.com>
2022-02-04 16:25:28 +0900

    MyService.java Method Override

--- app/src/main/java/com/moon/android_service_study/MyService.java
+++ app/src/main/java/com/moon/android_service_study/MyService.java
@@ -5,6 +5,21 @@ import android.content.Intent;
 import android.os.IBinder;
 
 public class MyService extends Service {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
     public MyService() {
     }
 
